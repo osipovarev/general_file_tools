f406ca374162ef780def2c85c6ab0c6ab113b791
Ekaterina Osipova <osipovarev@gmail.com>
2022-11-14 14:50:17 -0500

    added --ignore suffixes featujre to the script

--- replace_names_from_dict.py
+++ replace_names_from_dict.py
@@ -14,7 +14,7 @@ import sys
 __author__ = "Ekaterina Osipova, 2021."
 
 
-def read_anno_into_dict(anno, field, split_symbol):
+def read_anno_into_dict(anno, field, split_symbol, ignore):
     ## Reads an annotation file into a dictionary
 
     anno_dict = defaultdict(list)
@@ -30,7 +30,12 @@ def read_anno_into_dict(anno, field, split_symbol):
             else:
                 id = all_fields[field - 1]
                 transc_info = line.rstrip()
-                anno_dict[id].append(transc_info)
+                if ignore:
+                    id_suff = id.split('.')[-1]
+                    id_core = '.'.join(id.split('.')[:-1])
+                    anno_dict[id_core].append(transc_info)
+                else:
+                    anno_dict[id].append(transc_info)
     return anno_dict
 
 
@@ -50,6 +55,10 @@ def rename_ids(anno_dict, rename_table, prefix=False, suffix=False):
     for t in anno_dict:
         if t in rename_table:
             new_name = rename_table[t]
+            # if ignore:
+            #     id_suff = anno_dict[t][0]
+            #     new_name += ".{}".format(id_suff)
+            
             if prefix:
                 prefix = new_name + '_'
                 new_t = prefix + t.replace(prefix, '')
@@ -58,6 +67,7 @@ def rename_ids(anno_dict, rename_table, prefix=False, suffix=False):
                 new_t = t.replace(suffix, '') + suffix
             else:
                 new_t = new_name
+            
             for transc in anno_dict[t]:
                 print(transc.replace(t, new_t))
         else:
@@ -76,6 +86,7 @@ def main():
                         help='specify if you dont want to replace names entirely, just to add a suffix from the dictionary')
     parser.add_argument('-f', '--field', type=int, default=4, help='field number to label; default=4(transciprt_ID in bed12)')
     parser.add_argument('-c', '--comma', action='store_true', help='specify if anno file is comma separated (.csv)')
+    parser.add_argument('-i', '--ignore', action='store_true', help='specify if anno IDs have suffixes you want to ignore')
     args = parser.parse_args()
 
     ## bed12 format:
@@ -86,7 +97,7 @@ def main():
     split_symbol = ''
     if args.comma:
         split_symbol = ','
-    anno_dict = read_anno_into_dict(args.anno, args.field, split_symbol)
+    anno_dict = read_anno_into_dict(args.anno, args.field, split_symbol, args.ignore)
 
     ## Read file with name replacements
     rename_table = read_renaming_dict(args.name_dict)
