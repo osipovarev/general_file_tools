0ced1ca306d354c3de35a9c46d566c75bfca1266
Ekaterina Osipova <osipovarev@gmail.com>
2024-02-11 14:36:19 -0500

    added script to find best model based on AIC

---
+++ find_best_values.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+#
+import argparse
+from collections import defaultdict
+
+
+__author__ = "Ekaterina Osipova, 2024."
+
+
+def main():
+    ## Parse arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--input', type=str, help='table with values per ID: ID \t value1 \t value2 \t value3 ...')
+    parser.add_argument('-c', '--cutoff', type=int, default=2, help='the min difference between values to select one over another')
+    parser.add_argument('-max', '--max', action='store_true', help='specify if you want to search for max value; default: search for min')
+    args = parser.parse_args()
+
+    cutoff = args.cutoff
+
+    ## Parse input table
+    with open(args.input, 'r') as inf:
+        for line in inf.readlines():
+            ID = line.split()[0]
+            values = [float(i) for i in line.split()[1:]]
+
+            if args.max:
+                values = [-i for i in values]
+            for i, v in enumerate(values):
+                if i == 0:
+                    v_min = v
+                    n = i
+                else:
+                    if v_min - v > cutoff:
+                        v_min = v
+                        n = i
+            if args.max:
+                print('{}\t{}\t{}'.format(ID, -v_min, n))    
+            else:
+                print('{}\t{}\t{}'.format(ID, v_min, n))
+    		
+
+if __name__ == "__main__":
+    main()
